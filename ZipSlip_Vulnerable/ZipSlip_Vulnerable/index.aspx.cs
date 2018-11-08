3855b91a00258929f08112265279da6044d96719
ojmassam <ojmassam@gmail.com>
2018-11-08 04:52:18 +0200

    Vulnerability Demo Complete

---
+++ ZipSlip_Vulnerable/ZipSlip_Vulnerable/index.aspx.cs
@@ -0,0 +1,42 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ZipSlip_Vulnerable
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UploadVulnerable_Click(object sender, EventArgs e)
+        {
+            if (FileUploadControlV.HasFile)
+            {
+                try
+                {
+                    string filename = Path.GetFileName(FileUploadControlV.FileName);
+                    string saveDirectory = "../../Users/Public/Passwords/File/";
+                    string fileFilter = null;
+                    //FileUploadControlV.SaveAs(Server.MapPath("~/File/") + filename);
+
+                    FastZip badVulnerability = new FastZip();
+                    badVulnerability.ExtractZip(filename, saveDirectory, fileFilter);
+
+                    StatusVulnerable.InnerHtml = "File uploaded!";
+                }
+                catch (Exception ex)
+                {
+                    StatusVulnerable.InnerHtml = "The file could not be uploaded. The following error occured: " + ex.Message;
+                }
+            }
+        }
+    }
+}
---
+++ ZipSlip_Vulnerable/ZipSlip_Vulnerable/patched.aspx.cs
@@ -0,0 +1,49 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ZipSlip_Vulnerable
+{
+    public partial class patched : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UploadPatched_Click(object sender, EventArgs e)
+        {
+            if (FileUploadControlP.HasFile)
+            {
+                string message = "The file could not be uploaded. An error occured";
+                try
+                {
+                    string filename = Path.GetFileName(FileUploadControlP.FileName);
+                    string saveDirectory = "../../Users/Public/Passwords/File/";
+                    string fileFilter = null;
+                    //FileUploadControlP.SaveAs(Server.MapPath("~/File/") + filename);
+                    FastZip badVulnerability = new FastZip();
+                    badVulnerability.ExtractZip(filename, saveDirectory, fileFilter);
+
+                    if (!message.Equals("The file could not be uploaded. An error occured"))
+                    {
+                        StatusPatched.InnerHtml = message;
+                    }
+                    else
+                    {
+                        StatusPatched.InnerHtml = "File uploaded!";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    StatusPatched.InnerHtml = message;
+                }
+            }
+        }
+    }
+}
